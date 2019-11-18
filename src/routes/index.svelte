5961fd32641143412ef31db98287e1d6d3374e80
Nathan Shelly <nathan.shelly@opendoor.com>
2019-11-17 20:12:33 -0800

    Remove components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -61,17 +61,8 @@
 </style>
 
 <svelte:head>
-  <title>Sapper Blog Template</title>
+  <title>mostly dotfiles writings</title>
 </svelte:head>
 
 <div class="home-container">
-  <div class="home-copy">
-    <h1>Welcome to your new Sapper Blog</h1>
-    <p>Check out the docs on <a href="https://www.github.com/Charca/sapper-blog-template" target="_blank">GitHub</a> to get started.</p>
-  </div>
-
-  <figure>
-    <img alt='Person typing on laptop' src='undraw-illustration.svg'>
-    <figcaption>Illustration thanks to <a href="https://undraw.co" target="_blank">Undraw</a></figcaption>
-  </figure>
 </div>
