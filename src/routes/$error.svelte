4c84e033190c6f0ea555de7220ae651c71411ef5
Nathan Shelly <nsnshelly@gmail.com>
2021-04-04 22:11:12 -0700

    fix

--- src/routes/$error.svelte
+++ src/routes/$error.svelte
@@ -1,40 +1,38 @@
 <script lang="ts">
-  export let status: number
-  export let error: Error
+	export let status: number;
+	export let error: Error;
 
-  const dev = process.env.NODE_ENV === 'development'
+	import { dev } from '$app/env';
 </script>
 
-<style>
-  h1,
-  p {
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    font-weight: 700;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
-
-<svelte:head>
-  <title>{status}</title>
-</svelte:head>
+/* <svelte:head>*/ /* <title>{status}</title> */ /*</svelte:head> */
 
 <h1>{status}</h1>
 
 <p>{error.message}</p>
 
 {#if dev && error.stack}
-  <pre>{error.stack}</pre>
+	<pre>{error.stack}</pre>
 {/if}
+
+<style>
+	h1,
+	p {
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		font-weight: 700;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
